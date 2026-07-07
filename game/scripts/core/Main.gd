474bb9c0fa2f5e61a989c6cb73855b14d92dc33e
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-07 21:05:13 +0300

    Set first objective for game prototype

--- game/scripts/core/Main.gd
+++ game/scripts/core/Main.gd
@@ -3,7 +3,7 @@ extends Node
 func _ready() -> void:
     process_mode = Node.PROCESS_MODE_ALWAYS
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    ObjectiveManager.set_objective("Найти записку и проверить дверь.")
+    ObjectiveManager.set_objective("Запустить вечернюю сборку.")
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("close_message"):
