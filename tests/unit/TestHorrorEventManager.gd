e53a8c11ec31f01c4ab2da81612b7a07f4fae8fe
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-13 00:01:51 +0300

    fix: make legacy event test closures deterministic

--- tests/unit/TestHorrorEventManager.gd
+++ tests/unit/TestHorrorEventManager.gd
@@ -5,17 +5,19 @@ const ManagerScript = preload("res://game/scripts/core/HorrorEventManager.gd")
 
 func _init() -> void:
     var manager := ManagerScript.new()
-    var callback_count := 0
-    var started_count := 0
-    var finished_count := 0
+    var counters := {
+        "callback": 0,
+        "started": 0,
+        "finished": 0,
+    }
 
-    manager.horror_event_started.connect(func(_event_id: String) -> void: started_count += 1)
-    manager.horror_event_finished.connect(func(_event_id: String) -> void: finished_count += 1)
-    manager.play_once("test_event", func() -> void: callback_count += 1)
-    manager.play_once("test_event", func() -> void: callback_count += 1)
+    manager.horror_event_started.connect(func(_event_id: String) -> void: counters["started"] += 1)
+    manager.horror_event_finished.connect(func(_event_id: String) -> void: counters["finished"] += 1)
+    manager.play_once("test_event", func() -> void: counters["callback"] += 1)
+    manager.play_once("test_event", func() -> void: counters["callback"] += 1)
 
-    TestAssert.are_equal(callback_count, 1, "callback must run once")
-    TestAssert.are_equal(started_count, 1, "started signal must emit once")
-    TestAssert.are_equal(finished_count, 1, "finished signal must emit once")
+    TestAssert.are_equal(counters["callback"], 1, "callback must run once")
+    TestAssert.are_equal(counters["started"], 1, "started signal must emit once")
+    TestAssert.are_equal(counters["finished"], 1, "finished signal must emit once")
     TestAssert.is_true(manager.has_played("test_event"), "played event must be recorded")
     quit(0)
